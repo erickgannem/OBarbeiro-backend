ffc8b299961cb63915722c0a3ad421d86f64c4e0
Erick S. Gannem <erickgannem@gmail.com>
2020-07-03 16:33:41 -0400

    create placeholder routes for services, professionals, availability

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,14 @@ const { Router } = express;
 const routes = new Router();
 
 routes.get('/api', (req, res) => {
-  res.status(200).json({message: "Hello World"})
+  res.status(200).json({message: "Welcome to O'Barbeiro app API"})
 });
+
+routes.get('/api/services', (req, res) => res.status(200).json({message: "/api/services"}));
+
+routes.get('/api/professionals', (req, res) => res.status(200).json({message: "/api/professionals"}));
+
+routes.get('/api/availability', (req, res) => res.status(200).json({message: "/api/availability"}));
+
 
 export default routes;
