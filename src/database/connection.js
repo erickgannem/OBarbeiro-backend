771c9751fbdaf87088973c95bf4907e8b12b1b77
Erick Gannem <erickgannem@gmail.com>
2020-07-07 12:14:36 -0400

    Establish database connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,6 +5,12 @@ const environment = dotenv.config({path: "../../.env.dev"});
 
 mongoose.Promise = Promise;
 
-const { DB_USER, DB_NAME, DB_PASSWORD } = environment;
+const { DB_USER, DB_NAME, DB_PASSWORD } = environment.parsed;
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.xr7bk.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
+db.on('open', function() {
+  console.log('connected')
+})
